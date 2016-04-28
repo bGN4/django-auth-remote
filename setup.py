a20f7e06bbf3dc5eac35bb12e4cdc119bcfd5535
bGN4 <bGN4>
2016-04-28 12:08:45 +0800

    add static and setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+VERSION = __import__("auth_remote").__version__
+
+CLASSIFIERS = [
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+]
+
+install_requires = [
+]
+
+setup(
+    name="django-auth-remote",
+    description="subclass of django.contrib.auth.backends.RemoteUserBackend"
+            "to custom authenticate backend on remote server.",
+    version=VERSION,
+    author="bGN4",
+    author_email="git@github.com",
+    license='MIT License',
+    platforms=['OS Independent'],
+    url="https://github.com/bGN4/django-auth-remote",
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=install_requires,
+    classifiers=CLASSIFIERS,
+)
